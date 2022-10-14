89b728982b2325f53b681a66f0d7da0963760eae
alegonzo <alegonzo97@gmail.com>
2022-10-14 16:16:44 -0400

    clean

--- pages/games/[id].tsx
+++ pages/games/[id].tsx
@@ -2,11 +2,9 @@ import { useRouter } from 'next/router'
 import { GAMES } from '../../lib/data'
 import GameDetail from '../../components/Games/GameDetail'
 import useTranslation from 'next-translate/useTranslation'
-import { GetStaticPropsContext } from 'next'
 
 export default function GamePage() {
   const router = useRouter()
-  const { lang } = useTranslation('common')
   const { id } = router.query
   const game = GAMES.find((item) => item.id === id)
 
